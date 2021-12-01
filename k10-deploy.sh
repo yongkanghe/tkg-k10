72de8b7152c864143348c653fa1889f52a50532c
Yongkang He <he@yongkang.cloud>
2021-12-01 10:15:11 +0800

    Update k10-deploy.sh

--- k10-deploy.sh
+++ k10-deploy.sh
@@ -8,6 +8,7 @@ export AWS_ACCESS_KEY_ID=$(cat awsaccess | head -1)
 export AWS_SECRET_ACCESS_KEY=$(cat awsaccess | tail -1)
 
 tanzu cluster kubeconfig get $(cat tkg_wcluster_name) --admin
+kubectl config use-context $(kubectl config get-contexts -o name| grep $(cat tkg_wcluster_name))
 
 echo '-------Install K10'
 kubectl create ns kasten-io
